3f4ff30979eb437e27026f800ef18823ea422c5d
Joe Julian <me@joejulian.name>
2012-05-21 23:43:52 -0700

    This was completely broken. It should now handle the exceptions properly.

--- src/peer/detach.py
+++ src/peer/detach.py
@@ -1,23 +1,25 @@
 import subprocess
-
 def detach(hostname,remotehost="localhost"):
     """
 Detach ``host`` from the peer group. Failure will raise DetachError or DetachWarning.
 
 If ``remotehost`` is set, detach will be run on remote host.
 """
-    response = subprocess.check_output([
-            "/usr/sbin/gluster", 
-            "--remote-host=%s" % remotehost, 
-            "peer", 
-            "detach", 
-            hostname])
-    if response[-13:] == " is localhost":
-        raise DetachWarning, response
-    if response[-23:] == " is not part of cluster":
-        raise DetachWarning, response
-    if response[:23] == "Brick(s) with the peer ":
-        raise DetachWarning, response
-    if response[:36] == "Detach unsuccessful\nDetach returned ":
-        raise DetachError, response
-    return true
+    try:
+        response = subprocess.check_output([
+                "/usr/sbin/gluster", 
+                "--remote-host=%s" % remotehost, 
+                "peer", 
+                "detach", 
+                hostname])
+    except subprocess.CalledProcessError,e:
+        response = e.output
+        if response[-14:] == " is localhost\n":
+            raise Warning(response)
+        if response[-24:] == " is not part of cluster\n":
+            raise Warning(response)
+        if response[:23] == "Brick(s) with the peer ":
+            raise Warning(response)
+        if response[:36] == "Detach unsuccessful\nDetach returned ":
+            raise Exception(response)
+    return True
