0a161f9bc4dcc12c5cc81c5b4a8fb8b837b5fd8c
Joe Julian <me@joejulian.name>
2012-05-22 00:04:21 -0700

    Check for root permissions when loading module
    Fixes #2

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,18 @@
+class GlusterError(Exception):
+    def __init__(self,value):
+        self.value = value
+    def _str_(self):
+        return repr(self.value)
+
+class GlusterWarning(Warning):
+    def __init__(self,value):
+        self.value = value
+    def _str_(self):
+        return repr(self.value)
+
+import os,sys
+if not os.geteuid()==0:
+    raise GlusterError("Gluster commands require root permissions.")
+        
 import peer
 import volume
