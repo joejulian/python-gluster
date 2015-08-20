02beb7a30eb9f20b252b14dd5889e2f6aeb38a89
Joe Julian <jjulian@io.com>
2015-08-19 18:48:15 -0700

    Add license headers

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
+#    Copyright 2014, 2015 Joe Julian <me@joejulian.name>
+#
+#    This file is part of python-gluster.
+#
+#    python-gluster is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    python-gluster is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with python-gluster.  If not, see <http://www.gnu.org/licenses/>.
+#
 from distutils.core import setup
 
 setup(name='python-gluster',
         version='0.1',
+        license='GPLv3',
         package_dir={'gluster': 'src'},
         packages=[
             'gluster',
